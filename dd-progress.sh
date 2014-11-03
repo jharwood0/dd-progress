d227af22e4ec64f0c9bfdd091fb84f440a47d9e6
Josh Harwood <bocharwood@gmail.com>
2014-11-03 16:05:41 +0000

    added comments

--- dd-progress.sh
+++ dd-progress.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+#get every instance of dd and format them to CSV
 id=$(pgrep -d ',' '^dd$')
 
+#split the string into array
 IFS=',' read -a array <<< "$id"
 
 echo "Starting infinite loop"
 echo "ctrl-C to cancel"
 
+#forearch element in the array, send a USR1 signal to the process
+#loop continuously
 while(true)
 do
   for element in "${array[@]}"
@@ -15,5 +19,6 @@ do
     eval $command
     echo "sent signal to $element"
   done
+  #stops dos effect
   sleep 10
 done
